f3511ae19458a7c13e27fa369b4c558b28f41ba1
João Vitor <devjohncb@gmail.com>
2024-09-05 17:03:09 -0300

    feat: creates login functionality

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -10,5 +10,5 @@ export const authentication = (salt: string, password: string) => {
   return crypto
     .createHmac('sha256', [salt, password].join('/'))
     .update(SECRET)
-    .digest()
+    .digest('hex')
 }
