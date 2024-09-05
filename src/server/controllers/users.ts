44f1a0ea09c203162f9dc582c233d5a202805eb9
João Vitor <devjohncb@gmail.com>
2024-09-05 00:01:50 -0300

    feat: creates the authentication controller

--- src/server/controllers/users.ts
+++ src/server/controllers/users.ts
@@ -5,11 +5,11 @@ import { UserModel } from 'server/database/models/users'
 export const getUsers = () => UserModel.find()
 
 export const getUserByEmail = (email: string) => {
-  UserModel.findOne({ email })
+  return UserModel.findOne({ email })
 }
 
 export const getUserBySessionToken = (sessionToken: string) => {
-  UserModel.findOne({ 'authentication.sessionToken': sessionToken })
+  return UserModel.findOne({ 'authentication.sessionToken': sessionToken })
 }
 
 export const getUserById = (id: string) => UserModel.findById(id)
@@ -17,16 +17,16 @@ export const getUserById = (id: string) => UserModel.findById(id)
 // POST Method
 
 export const createUser = (values: Record<string, any>) => {
-  new UserModel(values).save().then((user) => user.toObject)
+  return new UserModel(values).save().then((user) => user.toObject)
 }
 
 // UPDATE/PATCH Method
 
 export const updateUserById = (id: string, values: Record<string, any>) => {
-  UserModel.findByIdAndUpdate(id, values)
+  return UserModel.findByIdAndUpdate(id, values)
 }
 // DELETE Method
 
 export const deleteUserById = (id: string) => {
-  UserModel.findByIdAndDelete({ _id: id })
+  return UserModel.findByIdAndDelete({ _id: id })
 }
